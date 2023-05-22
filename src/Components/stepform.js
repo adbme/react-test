8f73924680420f88992a364cc799f5e2f7da50cb
ad.bme <adil.bouamrioune@gmail.com>
2023-05-22 11:05:54 +0200

    complete stepform with file upload, drop down ...

--- src/Components/stepform.js
+++ src/Components/stepform.js
@@ -9,9 +9,22 @@ import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import '../form.css';
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
+
+
 const steps = ['name + surname + title', 'description + join files', 'date + hour + duration', 'rooms', 'dispositions'];
 
 export default function HorizontalLinearStepper() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
   const fileInputRef = useRef(null);
@@ -55,7 +68,9 @@ export default function HorizontalLinearStepper() {
     });
   };
 
+
   return (
+
     <body className='body'>
       <Box sx={{ bgcolor: '', height: '1000px', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <Box sx={{ width: '60%', height: '50%', bgcolor: 'white', padding: 2 }}>
@@ -152,23 +167,18 @@ export default function HorizontalLinearStepper() {
               {activeStep === 2 && (
                 <React.Fragment>
                   <Typography sx={{ mt: 2 }}>date :</Typography>
-                  <TextField
-                    placeholder="Add a description..."
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                  />
+
                   <Typography sx={{ mt: 2 }}>hour :</Typography>
                   <div className='hour'>
                     <TextField
-                      placeholder="00:00"
+                      placeholder="start"
                       variant="outlined"
                       fullWidth
                       margin="normal"
                     />
                     <Typography sx={{ mt: 2 }}>➡</Typography>
                     <TextField
-                      placeholder="00.00"
+                      placeholder="end"
                       variant="outlined"
                       fullWidth
                       margin="normal"
@@ -181,6 +191,94 @@ export default function HorizontalLinearStepper() {
                     fullWidth
                     margin="normal"
                   />
+                </React.Fragment>
+              )}
+              {activeStep === 3 && (
+                <React.Fragment>
+
+                  <Typography sx={{ mt: 2 }}>Room (s) :</Typography>
+                  <Box sx={{ minWidth: 120, mt: 2 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">SELECT ROOM(s)</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={age}
+                        label="Age"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={10}>Pleïades</MenuItem>
+                        <MenuItem value={20}>Suchet</MenuItem>
+                        <MenuItem value={40}>Chasseron</MenuItem>
+                        <MenuItem value={50}>Argentine</MenuItem>
+                        <MenuItem value={60}>Chamossaire</MenuItem>
+                        <MenuItem value={70}>Mönch</MenuItem>
+                        <MenuItem value={80}>Eiger</MenuItem>
+                        <MenuItem value={90}>Dent du midi</MenuItem>
+                        <MenuItem value={100}>Wengen</MenuItem>
+                        <MenuItem value={100}>Jungfrau</MenuItem>
+                        <MenuItem value={100}>Argentine + Chamossaire</MenuItem>
+                        <MenuItem value={100}>Chasseron + Suchet</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </React.Fragment>
+              )}
+
+              {activeStep === 4 && (
+                <React.Fragment>
+                  <Typography sx={{ mt: 2 }}>Disposition (optional) : :</Typography>
+
+                  <Box sx={{ minWidth: 120, mt: 2 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">CHOOSE DISPOSITION</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={age}
+                        label="Age"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={10}>1</MenuItem>
+                        <MenuItem value={20}>2</MenuItem>
+                        <MenuItem value={40}>3</MenuItem>
+                        <MenuItem value={50}>4</MenuItem>
+                        <MenuItem value={60}>5</MenuItem>
+                        <MenuItem value={70}>6</MenuItem>
+                        <MenuItem value={80}>7</MenuItem>
+                        <MenuItem value={90}>8</MenuItem>
+
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  <Typography sx={{ mt: 2 }}>Plan (optional) :</Typography>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                    onChange={(event) => {
+                      const selectedFiles = event.target.files;
+                      console.log(selectedFiles);
+                    }}
+                  />
+                  <Button sx={{ width: "100%", display: 'flex', alignItems: 'start', mb: 2 }}
+                    variant="outlined"
+                    onClick={() => {
+                      fileInputRef.current.click();
+                    }}
+                  >
+                    Upload image
+                  </Button>
+
+                  <Link
+                    component="button"
+                    to="/"
+                    onClick={() => {
+                      console.info("test");
+                    }}
+                  >
+                    VIEW DISPOSITIONS ➡
+                  </Link>
                 </React.Fragment>
               )}
               <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
